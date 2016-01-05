e130cc09fcf804ef6c016ca84ce518aa1b259af1
Nakiboudine MOHAMED <naki.md@gmail.com>
2016-01-05 01:20:11 +0100

    sanitize

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require('request')
 var http = require('http');
 var fs = require('fs');
 var Download = require('download');
+var sanitize = require("sanitize-filename");
 
 var artistPage = process.argv[2]
 
@@ -14,8 +15,8 @@ if (!_.startsWith(artistPage, 'http://'))
   process.exit(1)
 }
 
-request(artistPage, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+request(artistPage, function (err, response, body) {
+  if (!err && response.statusCode == 200) {
     var data = body.split('trackinfo : ')[1].split('}],')[0] + '}]'
     var parsed = JSON.parse(data)
     var baked = _.map(parsed, function (item) {
@@ -29,13 +30,13 @@ request(artistPage, function (error, response, body) {
       new Download({mode: '755'})
         .get(item.url)
         .rename({
-          basename: item.title,
+          basename: sanitize(item.title),
           extname: '.m4a'
         })
         .dest('./out')
         .run(function (err, files) {
-          if (err) throw err;
-          console.log(files[0].path + ' has been downloaded')
+          if (err) console.log(err);
+          else console.log(files[0].path + ' has been downloaded');
         })
     })
   }
